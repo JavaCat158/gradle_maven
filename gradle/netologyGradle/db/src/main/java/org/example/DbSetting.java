13ff485a51716d3f6ac1a3eb17025717c38dfb8c
Ildar <ibragimovildar229@gmail.com>
2023-09-11 18:09:28 +0300

    Initial commit

---
+++ gradle/netologyGradle/db/src/main/java/org/example/DbSetting.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class DbSetting {
+    private String name;
+    private String password;
+
+    DbSetting(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+}
